569e51e4b7b64e2c12232c5811cc80394a055190
Ov <>
2025-02-06 16:27:11 -0700

    Interfaz

---
+++ src/clase1/Clase1.java
@@ -0,0 +1,16 @@
+package clase1;
+
+import javax.swing.JPanel;
+
+public class Clase1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+       
+        // TODO code application logic here
+    }
+    
+}
